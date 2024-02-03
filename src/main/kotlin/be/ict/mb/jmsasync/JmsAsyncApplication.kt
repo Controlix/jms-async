4ddca03e35dce51e5f28adf7124f6520788d8066
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2024-02-03 20:04:18 +0100

    Roep http rest service aan tijdens verwerking

--- src/main/kotlin/be/ict/mb/jmsasync/JmsAsyncApplication.kt
+++ src/main/kotlin/be/ict/mb/jmsasync/JmsAsyncApplication.kt
@@ -2,10 +2,12 @@ package be.ict.mb.jmsasync
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.jms.annotation.EnableJms
 
 @SpringBootApplication
 @EnableJms
+@EnableFeignClients
 class JmsAsyncApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/be/ict/mb/jmsasync/MessageReceiver.kt
+++ src/main/kotlin/be/ict/mb/jmsasync/MessageReceiver.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
 
 @Component
-class MessageReceiver {
+class MessageReceiver(private val resourceClient: ResourceClient) {
 
     /**
      * Vaststelling:
@@ -17,6 +17,8 @@ class MessageReceiver {
     @JmsListener(destination = "numbers", concurrency = "5-5")
     fun receiveMessage(text: String) {
         LoggerFactory.getLogger(MessageReceiver::class.java).info("Start processing message {}", text)
+        val statsForMessage = resourceClient.findStatsForMessage("Kirk")
+        LoggerFactory.getLogger(MessageReceiver::class.java).info("Got stats for message {}: {}", "Kirk", statsForMessage)
         TimeUnit.MILLISECONDS.sleep(200)
         LoggerFactory.getLogger(MessageReceiver::class.java).info("Finished processing message {}", text)
     }
@@ -24,3 +26,18 @@ class MessageReceiver {
 
 // 2024-02-03T12:40:40.927+01:00  INFO 111984 --- [ntContainer#0-3] be.ict.mb.jmsasync.MessageReceiver       : Start processing message hello
 // 2024-02-03T12:40:45.092+01:00  INFO 111984 --- [ntContainer#0-4] be.ict.mb.jmsasync.MessageReceiver       : Finished processing message hello
+
+// 10 messages - no concurrency
+// 2024-02-03T19:53:40.328+01:00  INFO 122472 --- [ntContainer#0-1] be.ict.mb.jmsasync.MessageReceiver       : Start processing message hello
+// 2024-02-03T19:53:48.758+01:00  INFO 122472 --- [ntContainer#0-1] be.ict.mb.jmsasync.MessageReceiver       : Finished processing message hello
+// => 8.430s
+
+// 10 messages - concurrency = 5
+// 2024-02-03T19:57:13.918+01:00  INFO 122687 --- [ntContainer#0-2] be.ict.mb.jmsasync.MessageReceiver       : Start processing message hello
+// 2024-02-03T19:57:16.492+01:00  INFO 122687 --- [ntContainer#0-2] be.ict.mb.jmsasync.MessageReceiver       : Finished processing message hello
+// => 2.574s
+
+// 10 messages - concurrency = 5, rss bulkhead = 2
+// 2024-02-03T20:01:18.114+01:00  INFO 123200 --- [ntContainer#0-2] be.ict.mb.jmsasync.MessageReceiver       : Start processing message hello
+// 2024-02-03T20:01:23.263+01:00  INFO 123200 --- [ntContainer#0-5] be.ict.mb.jmsasync.MessageReceiver       : Finished processing message hello
+// => 5.149s
