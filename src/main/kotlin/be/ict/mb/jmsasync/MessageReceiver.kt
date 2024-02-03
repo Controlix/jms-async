c72fedad1735ea3f68aa62da7e588d3516cfcb1b
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2024-02-03 11:20:57 +0100

    Add artificial processing time

--- src/main/kotlin/be/ict/mb/jmsasync/MessageReceiver.kt
+++ src/main/kotlin/be/ict/mb/jmsasync/MessageReceiver.kt
@@ -1,13 +1,17 @@
 package be.ict.mb.jmsasync
 
+import org.slf4j.LoggerFactory
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
 class MessageReceiver {
 
     @JmsListener(destination = "numbers")
     fun receiveMessage(text: String) {
-        println(text)
+        LoggerFactory.getLogger(MessageReceiver::class.java).info("Start processing message {}", text)
+        TimeUnit.MILLISECONDS.sleep(200)
+        LoggerFactory.getLogger(MessageReceiver::class.java).info("Finished processing message {}", text)
     }
 }
